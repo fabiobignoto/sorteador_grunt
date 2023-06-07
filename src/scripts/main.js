36e38492aac9958a03e235427a00eee7845247ad
FabioBignoto <fabiopalacio14@gmail.com>
2023-06-07 10:37:00 -0300

    Commit Inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function(e) {
+        e.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = Math.floor((Math.random() * numeroMaximo)+1);
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+    })
+})
